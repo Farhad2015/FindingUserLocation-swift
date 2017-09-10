d5e1c5d1da2c06ba0005cc23a24c8cdac389bf9b
Mahmudur Rahman <mrforhad1@gmail.com>
2017-09-10 23:37:59 +0600

    Simple Update

--- Finding the user location/ViewController.swift
+++ Finding the user location/ViewController.swift
@@ -7,12 +7,20 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    var locationmanger = CLLocationManager()
+    @IBOutlet weak var map: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationmanger.delegate = self
+        locationmanger.desiredAccuracy = kCLLocationAccuracyBest
+        locationmanger.requestWhenInUseAuthorization()
+        locationmanger.startUpdatingLocation()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,27 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let userlocation: CLLocation = locations[0]
+        let latitude = userlocation.coordinate.latitude
+        let longitude = userlocation.coordinate.longitude
+        
+        let delta = 0.05
+        
+        let span = MKCoordinateSpan(latitudeDelta: delta, longitudeDelta: delta)
+        
+        let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        
+        let region = MKCoordinateRegion(center: location, span: span)
+        
+        self.map.setRegion(region, animated: true)
+        
+        let annotation = MKPointAnnotation()
+        annotation.title = "My Location"
+        annotation.coordinate = location
+        map.addAnnotation(annotation)
+        
+    }
 
 }
 
